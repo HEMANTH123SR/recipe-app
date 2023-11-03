ef03262e3c437e4e4a96bb8e83a93887acf1a9d5
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-03 14:53:30 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./components/Nav"
+
+const App=()=>{
+  return (
+  <div>
+    <Nav/>
+  </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+const Nav = () => {
+  return (
+    <div>
+      <nav className="w-full h-14 bg-green-600 flex justify-center items-center">
+        <h1 className="text-3xl font-mono font-bold text-slate-100">YumYum</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
