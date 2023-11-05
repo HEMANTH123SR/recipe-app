6277a6f1943f79dddf38194ee1d0b0905ac0a213
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-05 13:27:21 +0530

    added footer for DetailCard Components

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -6,6 +6,7 @@ import {
   Tags,
   IngredientAndAnalyzedInstructions,
 } from "./DetailCardComponents";
+import Footer from "./Footer";
 
 const DetailCard = () => {
   const location = useLocation();
@@ -33,7 +34,7 @@ const DetailCard = () => {
 
   useEffect(() => {
     fetch(
-      `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=31fae81620fb4678974ae21f891fe92b`
+      `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=f9b063217d4d42628c274ea08b034e9a`
     )
       .then((res) => res.json())
       .then((data) => dispatch(searchIdHandler(data)))
@@ -47,7 +48,8 @@ const DetailCard = () => {
       {error ? (
         <h1>Something went wrong: {error.message}</h1>
       ) : (
-        <div className="p-14 bg-slate-200 absolute">
+        
+        <div className="p-14 pb-0 bg-slate-200 absolute">
           <h3 className="absolute rounded-full p-5 text-3xl right-24 top-6 font-extrabold bg-green-600 text-slate-200 shadow-xl">
             {searchId.healthScore}
           </h3>
@@ -69,12 +71,18 @@ const DetailCard = () => {
           </div>
           
           <IngredientAndAnalyzedInstructions searchId={searchId} />
-        </div>
+         
+          <div className="mt-9">
+            
+            </div>
+            <Footer/>
+            </div>
+       
       )}
     </div>
   );
 };
 
-const IngredientsCard = ({ image, name, original, measures }) => {};
+
 
 export default DetailCard;
--- src/components/DetailCardComponents.jsx
+++ src/components/DetailCardComponents.jsx
@@ -43,12 +43,15 @@ export const Tags = ({ searchId }) => {
 
 export const IngredientAndAnalyzedInstructions = ({ searchId }) => {
   return (
-    <div className="flex flex-row space-x-9 w-full  mt-12">
+    <div className="flex flex-col space-x-9 w-full  mt-12">
       {/* Ingredients */}
-      <div className="h-full w-1/2 bg-slate-100 shadow-lg rounded-xl flex flex-col  justify-center items-center pb-3">
-        <h1 className="text-3xl text-green-600 underline font-bold m-4">
+      <div className="h-full  bg-slate-100 shadow-lg rounded-xl flex flex-col items-center   pb-3">
+        <h1 className="text-3xl text-green-600 underline font-bold m-8">
           Ingredients
         </h1>
+        <div className="grid grid-cols-2 w-4/5 ">
+
+      
         {searchId.extendedIngredients &&
         searchId.extendedIngredients.length > 1 ? (
           searchId.extendedIngredients.map((data) => {
@@ -64,13 +67,16 @@ export const IngredientAndAnalyzedInstructions = ({ searchId }) => {
         ) : (
           <h1>Error Ocuured with Extended Ingredients </h1>
         )}
+          </div>
       </div>
-      <div className="h-full w-2 bg-green-500"></div>
+     <div className="m-7"></div>
       {/* Instructions */}
-      <div className="h-full w-1/2 bg-slate-100 shadow-lg rounded-xl flex flex-col">
-        <h1 className="text-3xl text-green-600 underline font-bold m-4">
+      <div className="h-full bg-slate-100 shadow-lg rounded-xl flex flex-col items-center   pb-3" style={{margin:0}}>
+        <h1 className="text-3xl text-green-600 underline font-bold m-8">
           Instructions
         </h1>
+        
+        <div className="grid grid-cols-2 w-4/5">
         {searchId.analyzedInstructions  ? (
           searchId.analyzedInstructions[0].steps.map((data) => {
             return (
@@ -85,6 +91,7 @@ export const IngredientAndAnalyzedInstructions = ({ searchId }) => {
         ) : (
           <div>Error something went wrong with analyzed instructions </div>
         )}
+        </div>
       </div>
     </div>
   );
